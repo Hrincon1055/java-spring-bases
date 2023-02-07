c7256bcc6cc931c33148c751ef5c17a18b10bbb6
Henry Rincon <henryrinconsantana@gmail.com>
2023-02-06 19:59:43 -0500

    first commit

---
+++ src/main/java/com/udemy/springboot/web/app/controllers/IndexController.java
@@ -0,0 +1,45 @@
+package com.udemy.springboot.web.app.controllers;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.*;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import com.udemy.springboot.web.app.models.Usuario;
+
+@Controller
+@RequestMapping("/app")
+public class IndexController {
+	@GetMapping({ "/index", "/", "/home", "" })
+	public String index(Model model) {
+		model.addAttribute("titulo", "Curso spring");
+		return "index";
+	}
+
+	@GetMapping("/perfil")
+	public String perfil(Model model) {
+		Usuario usuario = new Usuario();
+		usuario.setNombre("Henry");
+		usuario.setApellido("Rincon");
+		usuario.setEmail("correo@correo.com");
+		model.addAttribute("titulo", "Curso spring ".concat(usuario.getNombre()));
+		model.addAttribute("usuario", usuario);
+		return "perfil";
+	}
+
+	@GetMapping("/listar")
+	public String listar(Model model) {
+		List<Usuario> usuarios = Arrays.asList(new Usuario("Andres", "Duarte", "test1@test.com"),
+				new Usuario("Felipe", "Coanon", "test2@test.com"), new Usuario("Henry", "Rincon", "test3@test.com"),
+				new Usuario("Juan", "Rincon", "test4@test.com"));
+		System.out.println(usuarios);
+		model.addAttribute("titulo", "Listado de usuarios");
+		model.addAttribute("usuarios", usuarios);
+		return "listar";
+	}
+
+}
